2f72581ef047c2ed84b8355f3139826b14eff23e
Frostmourne <cozroman@yandex.ru>
2020-04-20 11:36:35 +0300

    feat: demo page

--- src/js/index.ts
+++ src/js/index.ts
@@ -7,21 +7,35 @@ declare global {
   }
 }
 
-const testOptions = {
+const testOptions1 = {
   minValue: 0,
   maxValue: 100,
   step: 1,
-  defaultValue: [45, 85],
+  defaultValue: 60,
+}
+
+const testOptions2 = {
+  minValue: 0,
+  maxValue: 100,
+  step: 2,
+  defaultValue: [42, 88],
   isVertical: true,
   range: true
 }
-const testOptions1 = {
+
+const testOptions3 = {
+  minValue: 0,
+  maxValue: 100,
+  step: 5,
+  defaultValue: [20, 90],
+  range: true
+}
+const testOptions4 = {
   minValue: 0,
   maxValue: 100,
   step: 7,
   defaultValue: 45,
   isVertical: true,
-  // range: true
 }
 
 $.fn.slider = function (options: object) {
@@ -34,8 +48,12 @@ $.fn.slider = function (options: object) {
 }
 
 
-$('#root').slider(testOptions);
-$('#root-1').slider(testOptions1);
+$('#example-1').slider(testOptions1);
+$('#example-2').slider(testOptions2);
+$('#example-3').slider(testOptions3);
+$('#example-4').slider(testOptions4);
+
+
 
 $(() => {
 
@@ -50,8 +68,8 @@ $(() => {
   }
   
 
-  setTimeout(() => {
-    change(11);
-    changeRange([25, 70]);
-  }, 1000)
+  // setTimeout(() => {
+  //   change(11);
+  //   changeRange([25, 70]);
+  // }, 1000)
 })
