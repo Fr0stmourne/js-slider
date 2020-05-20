b715e0fbc74a43c0308f8412d7489aa63e0c8acc
Frostmourne <cozroman@yandex.ru>
2020-05-20 20:33:21 +0300

    refactor: get rid of commented code and update readme

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ function importAll(r) {
   r.keys().forEach(r);
 }
 
-// importAll(require.context('./vendors/', true, /\.js$/));
-// importAll(require.context('./components/', true, /\.js$/));
 importAll(require.context('./pages/', true, /\.(j|t)s$/));
 importAll(require.context('./', true, /\.scss$/));
 importAll(require.context('./', true, /\.(jpeg|jpg|png|gif|svg)$/));
