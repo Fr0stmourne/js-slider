23d01872d1029a81a13be4833d4ed1e72b74fd1f
Frostmourne <cozroman@yandex.ru>
2020-06-14 18:50:03 +0300

    chore: add tests for calculateSteps

---
+++ src/components/plugin/utils/calculateSteps/calculateSteps.test.ts
@@ -0,0 +1,19 @@
+import calculateSteps from './calculateSteps';
+
+describe('calculateSteps()', () => {
+  test('should correctly calculate steps', () => {
+    const minValue = 0;
+    const maxValue = 100;
+    const step = 25;
+
+    expect(calculateSteps(minValue, maxValue, step)).toStrictEqual([0, 25, 50, 75, 100]);
+  });
+
+  test('should correctly handle case when min/max value is not multiple of the step', () => {
+    const minValue = -1;
+    const maxValue = 100;
+    const step = 25;
+
+    expect(calculateSteps(minValue, maxValue, step)).toStrictEqual([-1, 24, 49, 74, 99, 100]);
+  });
+});
