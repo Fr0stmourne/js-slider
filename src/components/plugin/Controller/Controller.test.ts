0cfcede15a2dc4707342ab113949a96028d991f8
Frostmourne <cozroman@yandex.ru>
2020-06-15 20:11:00 +0300

    fix: tests update

--- src/components/plugin/Controller/Controller.test.ts
+++ src/components/plugin/Controller/Controller.test.ts
@@ -3,15 +3,26 @@ import Model from '../Models/Model';
 import Controller from './Controller';
 import { DEFAULT_VIEW_STATE, DEFAULT_MODEL_STATE } from '../defaults';
 
-jest.mock('../Views/View/View');
-
+let controller: Controller;
+let view: View;
+let model: Model;
 describe('Controller constructor', () => {
+  beforeEach(() => {
+    view = new View(DEFAULT_VIEW_STATE, DEFAULT_MODEL_STATE);
+    model = new Model(DEFAULT_MODEL_STATE);
+    controller = new Controller(model, view);
+  });
   test('should store passed model and view', () => {
-    const view = new View(DEFAULT_VIEW_STATE, DEFAULT_MODEL_STATE);
-    const model = new Model(DEFAULT_MODEL_STATE);
-    const controller = new Controller(model, view);
-
     expect(controller.view).toEqual(view);
     expect(controller.model).toEqual(model);
+  });
+});
+
+describe('setUserCallback', () => {
+  test('should store passed user callback', () => {
+    const userCallback = jest.fn();
+    controller.setUserCallback(userCallback);
+
+    expect(controller.userCallback).toStrictEqual(userCallback);
   });
 });
