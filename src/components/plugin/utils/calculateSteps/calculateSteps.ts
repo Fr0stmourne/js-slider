3e737e79e5d0763deb1425dbc5716cc627e3e916
Frostmourne <cozroman@yandex.ru>
2020-06-18 13:31:16 +0300

    #11 replace for with reduce

--- src/components/plugin/utils/calculateSteps/calculateSteps.ts
+++ src/components/plugin/utils/calculateSteps/calculateSteps.ts
@@ -7,10 +7,12 @@ export default function calculateSteps({
   maxValue: number;
   step: number;
 }): number[] {
-  const steps = [];
-  for (let i = minValue; i < maxValue; i += step) {
-    steps.push(i);
-    if (i + step >= maxValue) steps.push(maxValue);
-  }
-  return steps;
+  const emptySteps = Array(Math.floor((maxValue - minValue) / step) + 1).fill(null);
+  return emptySteps.reduce((acc: number[], el: number, index: number) => {
+    acc.push(Math.min(minValue + index * step, maxValue));
+    const hasMaxValue = acc.includes(maxValue);
+    const nextGreaterThanMax = minValue + (index + 1) * step > maxValue;
+    if (nextGreaterThanMax && !hasMaxValue) acc.push(maxValue);
+    return acc;
+  }, []);
 }
