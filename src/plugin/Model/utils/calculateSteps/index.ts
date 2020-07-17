3ed00c108ef6479c9e2e608cf1b94f49fb3c326b
Frostmourne <cozroman@yandex.ru>
2020-07-17 11:19:11 +0300

    rework calculateStep function #17

--- src/plugin/Model/utils/calculateSteps/index.ts
+++ src/plugin/Model/utils/calculateSteps/index.ts
@@ -1,4 +1,5 @@
 function calculateSteps({ minValue, maxValue, step }: { minValue: number; maxValue: number; step: number }): number[] {
+  if (Math.abs(maxValue - minValue) < 2 * step) return [minValue, maxValue];
   const emptySteps = Array(Math.floor((maxValue - minValue) / step) + 1).fill(null);
   return emptySteps.reduce((acc: number[], el: number, index: number) => {
     acc.push(Math.min(minValue + index * step, maxValue));
